6959f1a1e779aa311e82fbeba76bfc8efb503ddf
Jahid <jahid16@cse.pstu.ac.bd>
2025-04-27 15:18:31 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="usa_visa",
+    version="0.0.0",
+    author="Jahid",
+    author_email="jjahid1559@gmail.com",
+    packages=find_packages()
+)
