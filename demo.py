11137bb289ff8fc13eb8fcf7fb0a16754eca668d
Jahid <jahid16@cse.pstu.ac.bd>
2025-05-03 11:13:11 +0600

    model trainer updated

--- demo.py
+++ demo.py
@@ -1,13 +1,4 @@
-from pymongo import MongoClient
-import os
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
-# If you used export, you can access it like this:
-mongo_url = os.getenv("MONGODB_URL")
-
-try:
-    client = MongoClient(mongo_url)
-    # Test the connection by listing databases
-    print("Connected! Databases:")
-    print(client.list_database_names())
-except Exception as e:
-    print("Connection failed:", e)
+obj = TrainPipeline()
+obj.run_pipeline()
